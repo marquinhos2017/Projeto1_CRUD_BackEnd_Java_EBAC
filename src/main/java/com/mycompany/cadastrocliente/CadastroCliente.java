d04a16f02ce48a9c8d287b6ebfaf0a10578d7bb0
Marcos Rodrigues <marcosrodriguescorreiajc@gmail.com>
2024-06-27 13:21:12 +0100

    first commit

---
+++ src/main/java/com/mycompany/cadastrocliente/CadastroCliente.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cadastrocliente;
+
+/**
+ *
+ * @author marcosrodriguescorreia
+ */
+public class CadastroCliente {
+
+    public static void main(String[] args) {
+      TelaPrincipal tela = new TelaPrincipal();
+      tela.setVisible(true);
+      
+            
+    }
+}
